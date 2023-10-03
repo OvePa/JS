a2f992a3e17aa1739d7f15f8e1a8bf543e8f762b
OvePaz <omar.velazquez.paz@gmail.com>
2023-10-03 14:17:09 -0600

    .

---
+++ DS/Queue.js
@@ -0,0 +1,27 @@
+module.exports = class Queue {
+  constructor() {
+    this.items = new DoublyLinkedList();
+  }
+
+  isEmpty() {
+    return this.items.length == 0;
+  }
+
+  getFront() {
+    if (!this.isEmpty()) {
+      return this.items.getHead();
+    } else return null;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  enqueue(element) {
+    return this.items.insertTail(element);
+  }
+
+  dequeue() {
+    return this.items.removeHead();
+  }
+};
